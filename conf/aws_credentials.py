915fd5c3fb0568d8aea495d80e669a35f26d95dc
Indira Nellutla <indira@qxf2.com>
2018-11-28 13:18:47 +0530

    Removed aws_credentials.py

--- conf/aws_credentials.py
+++
@@ -1,8 +0,0 @@
-"""
-This config file would have the AWS credentials 
-"""
-
-aws_access_key_id= 'Enter the AWS access key here'	
-aws_secret_access_key='Enter the AWS secret key here'
-region='Enter the region'
-
